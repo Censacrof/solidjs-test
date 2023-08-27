23cad01086321d51853c3a7233a70e15523cd976
Francesco Galisi <galisifrancesco@gmail.com>
2023-08-27 21:22:27 +0200

    Remove padding from body

--- src/root.tsx
+++ src/root.tsx
@@ -27,7 +27,7 @@ export default function Root() {
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Body class="bg-zinc-950 p-4">
+      <Body class="bg-zinc-950">
         <Suspense>
           <ErrorBoundary>
             <Routes>

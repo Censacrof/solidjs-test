3cc99c2f811f783fafabf14680d65325a43162c9
Francesco Galisi <galisifrancesco@gmail.com>
2023-08-27 20:56:48 +0200

    Run prettier

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,14 +2,14 @@ import { TodoList } from "~/components/TodoList";
 
 export default function Home() {
   return (
-      <main class="fixed inset-0 grid grid-cols-main grid-rows-main">
-        <div class="row-start-2 col-start-2 flex flex-col bg-zinc-900 rounded-xl text-zinc-100 p-4 gap-4">
-          <div class="flex flex-col items-center">
-            <p class="text-xl">yet another</p>
-            <h1 class="text-5xl">Todo list!</h1>
-          </div>
-          <TodoList />
+    <main class="fixed inset-0 grid grid-cols-main grid-rows-main">
+      <div class="row-start-2 col-start-2 flex flex-col bg-zinc-900 rounded-xl text-zinc-100 p-4 gap-4">
+        <div class="flex flex-col items-center">
+          <p class="text-xl">yet another</p>
+          <h1 class="text-5xl">Todo list!</h1>
         </div>
+        <TodoList />
+      </div>
     </main>
   );
 }

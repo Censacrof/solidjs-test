91d09a3f1ca6a4560c5a18e1aa08a1807170e718
Francesco Galisi <galisifrancesco@gmail.com>
2023-08-28 01:36:29 +0200

    Use Navigate decleratively

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,19 +5,19 @@ import { TodoList } from "~/components/TodoList";
 export default function Home() {
   const { accessToken } = useSession();
 
-  if (!accessToken()) {
-    return <Navigate href="./login" />;
-  }
-
   return (
-    <main class="fixed inset-0 grid grid-cols-main grid-rows-main">
-      <div class="row-start-2 col-start-2 flex flex-col bg-zinc-900 rounded-xl text-zinc-100 p-4 gap-4">
-        <div class="flex flex-col items-center">
-          <p class="text-xl">yet another</p>
-          <h1 class="text-5xl">Todo list!</h1>
+    <>
+      {!accessToken() && <Navigate href="./login" />}
+
+      <main class="fixed inset-0 grid grid-cols-main grid-rows-main">
+        <div class="row-start-2 col-start-2 flex flex-col bg-zinc-900 rounded-xl text-zinc-100 p-4 gap-4">
+          <div class="flex flex-col items-center">
+            <p class="text-xl">yet another</p>
+            <h1 class="text-5xl">Todo list!</h1>
+          </div>
+          <TodoList />
         </div>
-        <TodoList />
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,51 +1,49 @@
 import { createSignal } from "solid-js";
-import { useNavigate } from "solid-start";
+import { Navigate } from "solid-start";
 import { useSession } from "~/SessionContext";
 import { Button } from "~/components/Button";
 import { Input } from "~/components/Input";
 
 export default function Login() {
-  const navigate = useNavigate();
-  const { setAccessToken } = useSession();
+  const { accessToken, setAccessToken } = useSession();
 
   const [email, setEmail] = createSignal("");
   const [password, setPassword] = createSignal("");
 
   return (
-    <div class="fixed inset-0 flex items-center justify-center">
-      <div class="flex flex-col w-modal h-modal bg-zinc-900 p-4 rounded-lg gap-4">
-        <div class="flex-grow flex flex-col bg-zinc-800 rounded-md p-2 justify-evenly">
-          <label class="flex flex-col gap-1">
-            <span class="text-zinc-50">e-mail</span>
-            <Input
-              type="email"
-              placeholder="eg. foo@bar.com"
-              value={email()}
-              onChange={(event) => setEmail(() => event.target.value)}
-            />
-          </label>
-          <label class="flex flex-col gap-1">
-            <span class="text-zinc-50">password</span>
-            <Input
-              type="password"
-              placeholder="enter your password"
-              value={password()}
-              onChange={(event) => setPassword(() => event.target.value)}
-            />
-          </label>
-          <a href="#" class="text-blue-400">
-            I forgot my password
-          </a>
+    <>
+      {accessToken() && <Navigate href={"/"} />}
+
+      <div class="fixed inset-0 flex items-center justify-center">
+        <div class="flex flex-col w-modal h-modal bg-zinc-900 p-4 rounded-lg gap-4">
+          <div class="flex-grow flex flex-col bg-zinc-800 rounded-md p-2 justify-evenly">
+            <label class="flex flex-col gap-1">
+              <span class="text-zinc-50">e-mail</span>
+              <Input
+                type="email"
+                placeholder="eg. foo@bar.com"
+                value={email()}
+                onChange={(event) => setEmail(() => event.target.value)}
+              />
+            </label>
+            <label class="flex flex-col gap-1">
+              <span class="text-zinc-50">password</span>
+              <Input
+                type="password"
+                placeholder="enter your password"
+                value={password()}
+                onChange={(event) => setPassword(() => event.target.value)}
+              />
+            </label>
+            <a href="#" class="text-blue-400">
+              I forgot my password
+            </a>
+          </div>
+          <Button onClick={() => setAccessToken("a valid access token")}>
+            Login
+          </Button>
         </div>
-        <Button
-          onClick={() => {
-            setAccessToken("a valid access token");
-            navigate("/");
-          }}
-        >
-          Login
-        </Button>
       </div>
-    </div>
+    </>
   );
 }

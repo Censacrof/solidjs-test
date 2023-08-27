cae810764a64717b342f81835c3bbff84897b668
Francesco Galisi <galisifrancesco@gmail.com>
2023-08-27 20:19:50 +0200

    Move background to root

--- src/root.tsx
+++ src/root.tsx
@@ -27,7 +27,7 @@ export default function Root() {
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Body>
+      <Body class="bg-zinc-950 p-4">
         <Suspense>
           <ErrorBoundary>
             <Routes>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,8 +2,7 @@ import { TodoList } from "~/components/TodoList";
 
 export default function Home() {
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <div class="fixed inset-0 grid grid-cols-main grid-rows-main bg-zinc-950">
+      <main class="fixed inset-0 grid grid-cols-main grid-rows-main">
         <div class="row-start-2 col-start-2 flex flex-col bg-zinc-900 rounded-xl text-zinc-100 p-4 gap-4">
           <div class="flex flex-col items-center">
             <p class="text-xl">yet another</p>
@@ -11,7 +10,6 @@ export default function Home() {
           </div>
           <TodoList />
         </div>
-      </div>
     </main>
   );
 }
